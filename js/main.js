9db3ca5b2f30ff280d32604f48f56a1029c50947
Ovayo Monti <o.monti@FOD-L7VYF8J2.local>
2017-11-01 20:22:58 +0200

    intergrating js to html

--- js/main.js
+++ js/main.js
@@ -3,159 +3,69 @@ var user = {
 	lastName: 'Monti',
 	emailAddress: 'montiovayo@gmail.com',
 	age: 31,
+	salary: 15000,
 	children: [{
 		name: 'Ave',
-		childlastName: 'Monti',
-		age: 3
+		age: 3,
+		amount: 1000
 	}, {
 		name: 'Aphelele',
-		childlastName: 'Monti',
-		age: 23
-	}
-	, {
+		age: 18,
+		amount: 1500
+	}, {
 		name: 'Aviwe',
-		childlastName: 'Monti',
-		age: 29
+		age: 29,
+		amount: 2000
 	}]
 };
 
-console.log(user.firstName); //'Ovayo'
-console.log(user.lastName);  //'Monti'
+function percentageNumber(number, percent) {
+	return (percent / 100) * number
+}
 
-console.log(user.children);         //[{name: 'Ave', age: 3}, {name: 'Aphelele', age: 23}, {name: 'Aviwe', age: 29}]
-console.log(user.children[0]);      //{name: 'Ave', age: 3}
-console.log(user.children[1].name); //'Aphelele'
+function maintenanceDeduction(user) {
+	// Loop through the user's children
+	   // If child is younger than 18,
+	   // add the amount to the total maintenance amount
+	// Return the total maintenance amount
 
-console.log(user.firstName + user.lastName); //'OvayoMonti'
-
-console.log(user.firstName + ' ' + user.lastName); //'Ovayo Monti'
-console.log(`${user.firstName} ${user.lastName}`); //'Ovayo Monti'
-
-console.log(1 + 1);   //2
-console.log('1 + 1'); //'1 + 1'
-console.log(`${0 + 1} + ${101 - 100}`); //'1 + 1'
-
-user.childName = user.children.name + ' ' + user.children.childlastName;
-user.name = user.firstName + ' ' + user.lastName;
-
-function greetUser(firstName) {
-	if(firstName) {
-		alert('Hello, ' + firstName);
-	} else {
-		console.log('No firstName was specified');
+	if(!user) {
+		return;
 	}
-}
 
-function sumNumber(number=0) {
-	alert(127 + number);
-}
+	var maintenanceAmount = 0;
 
-function percentageNumber(thisIs, percentOf) {
-	return (thisIs / percentOf) * 100
-}
+	if(user.children && typeof(user.children) == 'object') {
+		console.log('--- Inclusion on if');
+		for(i=0;i<user.children.length;i++) {
+			console.log('--- Within loop. Index: ' + i);
+			var child = user.children[i];
 
-var price = 134754.65,
-    AgeMin = 50,
-    AgeMax = 65,
-    taxBreakOnAgeMin = 10,
-    taxBreakOnAgeMax = 20,
-    taxBreakOnChildren = 10;
+			if(child.age && child.amount) {
+				if(child.age < 18) {
+					maintenanceAmount += child.amount
+				}
+			}
 
-var discount = 0,
-		totalPrice = price;
-
-// if ( (user.age >= oldAge) && () user.children.length > 0 ) {
-// 	var totalPrice = price - taxBreakOnAge - taxBreakOnChildren
-// } else if (user.age >= oldAge) {
-// 	var totalPrice = price - taxBreakOnAge
-// } else {
-// 	var totalPrice = price
-// }
-
-// if (user.age >= AgeMax) {
-// 	totalPrice = totalPrice - taxBreakOnAgeMax;
-// }
-
-// if (user.age >= AgeMin && user.age < AgeMax) {
-// 	totalPrice = totalPrice - taxBreakOnAgeMin;
-// }
-
-if (user.age >= AgeMax) {
-	totalPrice = totalPrice - taxBreakOnAgeMax;
-} else if (user.age >= AgeMin) {
-	totalPrice = totalPrice - taxBreakOnAgeMin;
-}
-
-alert(totalPrice + ' a saving of ' + (price - totalPrice));
-
-totalPrice = totalPrice - ( user.children.length * taxBreakOnChildren )
-
-alert(totalPrice + ' a saving of ' + (price - totalPrice));
-
-// if (user.children.length > 0) {
-// 	totalPrice = totalPrice - taxBreakOnChildren;
-// }
-
-// if(user.lastName == 'Monti') {
-// 	alert('Xhosa');
-// } else if ( user.lastName == 'Mkhiza') {
-// 	alert('Youre High');
-// } else {
-// 	alert('Sober');
-// }
-
-function lang(eng) {
-	if(user.lastName == 'Monti') {
-		alert('Hi');
-	} else if ( user.lastName == 'Mkhize') {
-		alert('Hello');
+			console.log(child);
+		}
 	} else {
-		alert('Bye');
+		console.log('--- Exclusion on if');
 	}
+
+	return maintenanceAmount;
 }
 
-// switch statement session
-switch(user.age) {
-  case 30:
-  	console.log('30');
-    break;
-  case 31:
-  	console.log('31');
-    break;
-  case 35:
-  	console.log('35');
-    break;
-  default:
-  	console.log('Not 30, 31 or 35')
+function calculateDeductions(user) {
+	return maintenanceDeduction(user) + percentageNumber(user.salary, 14)
 }
 
-alert('About to break')
+var salary = user.salary,
+		deductions = calculateDeductions(user),
+		salaryAfterDeductions = salary - deductions;
 
-//While Loops Session
-var siblings = [
-	'Ave',
-	'Aphelele',
-	'Aviwe'
-];
+alert('Get ready');
 
-// var count = 0;
-// while(count < invoiceItems) {
-// 	console.log(count);
+var salaryDiv = document.getElementById('userSalary');
 
-// 	// currentValue = currentValue + 1
-// 	// currentValue += 1
-// 	count++
-// }
-
-for(i=0;i<siblings.length;i++) {
-	console.log( siblings + ' are ' + user.name  + "'s" + ' Kids ' );
-}
-
-
-// for(i=0;i<user.children.length;i++) {
-// 	console.log( user.children[i].name + ' are ' + user.name  + "'s" + ' Kids ' );
-// }
-
-
-
-
+salaryDiv.innerHTML = salary;
